cf5597c810795bebc7e57e4a7a832146c7f91ea7
Vishnu Vijayan <vvijayan@purdue.edu>
2017-10-07 17:13:56 -0400

    Set up home page and router

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+/* Angular Material */
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { PhysicianComponent } from './components/physician/physician.component';
+import { PharmacistComponent } from './components/pharmacist/pharmacist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PhysicianComponent,
+    PharmacistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, MatIconModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'physician',
+        component: PhysicianComponent
+      },
+      {
+        path: 'pharmacist',
+        component: PharmacistComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-client/src/app/components/home/home.component.ts
+++ ng-client/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +12,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    particlesJS.load('particles-js', 'assets/js/particles.json', null);
   }
 
 }
